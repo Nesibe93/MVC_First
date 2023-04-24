837ca834eeb07cad78a6a44671812b8c5f7ff74f
Nesibe93 <125722258+Nesibe93@users.noreply.github.com>
2023-04-24 11:11:35 +0300

    Add project files.

---
+++ MVCViews/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCViews.Models;
+using System.Diagnostics;
+
+namespace MVCViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet] // HttpGet metoduyla sayfayı getiriyoruz
+        public IActionResult Personel()
+        {
+            return View();
+        }
+
+        [HttpPost] // HttpPost metoduyla sayfadan gelecek olan verileri alabiliyoruz
+        public IActionResult Personel(Personel personel)
+        {
+            if (ModelState.IsValid) // ModelState : Modelim geçerli ise?
+            {
+                string personelBilgi = " ";
+                personelBilgi = "Personel Bilgileri(Ad,Soyad,Yas): "+ personel.Ad + " " + personel.Soyad + " " + personel.Yas.ToString();
+                ViewBag.Mesaj = personelBilgi;
+            }
+            return View(personel);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCViews/Models/Personel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCViews.Models
+{
+    public class Personel
+    {
+        public string Ad { get; set; }
+        public int Soyad { get; set; }
+        public int Yas { get; set; }
+    }
+}
